624ebc284049a090f00d152fca86943f4d74a3c6
alisilakg <wisper14@yandex.ru>
2023-03-24 21:15:14 +0500

    без дополнительного задания

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+
+@SpringBootTest
+class FilmorateApplicationTests {
+
+}
